e1d15da588054cb4ae156c22b1118f89611ab055
Alfredo Gonzalez <56003346+Dakoth@users.noreply.github.com>
2021-02-06 21:46:13 -0800

    implemented puzzle choices

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <queue>
 #include <vector> 
 
+#include <sstream>
+
 using namespace std;
 
 int numOfNodes = 0; //global variable to count number of nodes
@@ -24,7 +26,7 @@ class Node {
 
 
 class Problem {
-    int goalState[9] = {1, 2, 3, 4, 5, 6, 7, 8, 0};   //2D representation of the goalState 
+    vector<int> goalState = {1, 2, 3, 4, 5, 6, 7, 8, 0};   //2D representation of the goalState 
 
 
 };
@@ -36,34 +38,71 @@ class Problem {
 
 //main program
 int main() {
-    char user; 
+    char user = '\0';
+    string userPuzzle = "";  
+    string userPuzzleConcat = "";
+    //int problem[9] = {}; //Initalize an empty problem 
+    vector<int> problem;
+
+    //int defaultProb[9] = {1, 2, 3, 4, 8, 0, 7, 6, 5};
+    vector<int> defaultProb = {1, 2, 3, 4, 8, 0, 7, 6, 5};
+
     cout << "Hello and Welcome to Alfredo Gonzalez' 8-puzzle solver " << endl;
     cout << "Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle." << endl;
     
-    //FIXME
+    //Puzzle choices
     while (user != '1' && user != '2') { 
         cin >> user; 
-        if (user == '1' ) { 
-            cout << "IMPLEMENT DEFAULT" << endl;
+        cin.ignore(); 
+
+        if (user == '1' ) { //default puzzle 
+            problem = defaultProb;
         }
-        
-        else if (user == '2') {
-            cout << "IMPLEMENT OWN PUZZLE" << endl; 
+        else if (user == '2') { //If not the default
+            cout << "Enter your puzzle, use a zero to represent the blank" << endl;
+            cout << "Enter the first row, use space or tabs between numbers" << endl; 
+
+            getline(cin, userPuzzle);
+            userPuzzleConcat += userPuzzle + " ";
+            //cout << userPuzzleConcat << endl;// test
+
+            cout << "Enter the second row, use space or tabs between numbers" << endl;
+            getline(cin, userPuzzle);
+            userPuzzleConcat += userPuzzle + " ";
+            //cout << userPuzzleConcat << endl;// test
+
+            cout << "Enter the third row, use space or tabs between numbers" << endl;
+            getline(cin, userPuzzle);
+            userPuzzleConcat += userPuzzle;
+
+            //testing putting the streamstring in thingy 
+            stringstream ss(userPuzzleConcat);
+            int n;
+            char ch;
+            
+            while(ss >>n ){
+                problem.push_back(n);
+            }
+            ss.clear();
         }
         else {
-            cout << "pick a right answer bruh" << endl;
+            cout << "Plese pick a valid input " << endl;
         }
-        //user = '';
     }   
 
+     //tests if the array was added 
+    for(int i = 0; i < problem.size(); i++){ 
+         cout << problem.at(i) << " ";
+    }
     //testing out queues 
+    /*
     queue<int> testQ;
     testQ.push(5);  
     cout << "the front of the queue is: " << testQ.front() << endl;
     cout << endl;
+    */
 
-
-
+   
     //Testing out 2d arrays, maybe could use this for mismatched heuristic 
     char goalState[3][3] = {{'1', '2', '3'},
                             {'4', '5', '6'},
@@ -80,6 +119,7 @@ int main() {
 
 
     int mismatches = 0; 
+    /*
     //can make the '3' just an n value
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
@@ -93,6 +133,6 @@ int main() {
         }
     }            
     cout << "The number of mismatches is: " << mismatches << endl;        
-
+    */
     return 0; 
 }
